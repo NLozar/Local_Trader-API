aa3b4c55a56e04175275f9f411a61f9672901b1f
NLozar <nejc.lozar.slo@gmail.com>
2022-07-27 01:29:57 +0200

    user registration implemented

--- DBHandler.py
+++ DBHandler.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from decouple import config
+import bcrypt
 
 class DBHandler:
     def __init__(self, user, pw, host, port, db):
@@ -36,8 +37,22 @@ class DBHandler:
         for i, key in enumerate(keys):
             item_details[key] = res[i+OFFSET]
         return item_details
+    
+    def register_user(self, username, pw, uuid):
+        sql = "insert into users (username, pw, uuid) values (%s, %s, %s)"
+        val = (username, pw, uuid)
+        self.cursor.execute(sql, val)
+        self.db.commit()
+
+    def get_all_usernames(self):
+        self.cursor.execute("select username from users;")
+        usernames = []
+        for i in self.cursor:
+            usernames.append(i)
+        usernames = [i for ii in usernames for i in ii]
+        return usernames
 
 # MAIN (testing only)
 if __name__ == "__main__":
     db = DBHandler("root", config("MYSQL_PW"), "localhost", 13306, "local_trader")
-    db.get_all_items()
+    print("pajdo" in db.get_all_usernames())
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, jsonify, request
 from flask_restful import Api
 from DBHandler import DBHandler
 from decouple import config
 import traceback
 import json
+import bcrypt
+import uuid
 
 PORT = 14443
 
@@ -30,6 +32,25 @@ def get_item_details(uuid):
         print(traceback.print_exc())
         return Response(json.dumps({"message": "internal server error"}), status=500, mimetype="application/json")
     return jsonify(details)
+
+@app.route("/registerUser", methods=["POST"])
+def register_user():
+    try:
+        username = request.headers["username"]
+        password = request.headers["password"]
+    except Exception:
+        return Response(json.dumps({"message": "username or password retrival failed"}), status=400, mimetype="application/json")
+    try:
+        usernames = db.get_all_usernames()
+        if username in usernames:
+            return Response(json.dumps({"message": f"Username '{username}' is already taken."}), status=409, mimetype="application/json")
+    except Exception:
+        return Response(json.dumps({"message": "internal server error"}), status=500, mimetype="application/json")
+    try:
+        db.register_user(username, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), str(uuid.uuid4()))
+    except Exception:
+        return Response(json.dumps({"message": "registering a user into database failed"}), status=500, mimetype="application/json")
+    return Response(json.dumps({"message": "User registered"}), status=204, mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT, ssl_context=("certs/cert.pem", "certs/key.pem"))
